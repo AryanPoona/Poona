a228a99dbb7b76bccd1c6a0d61299732d6c11ce5
aryanpoona <aryanpoona032@gmail.com>
2018-12-25 07:25:17 +0530

    This is my first Local Repository Commit

---
+++ MysqlCon/src/com/MysqlConnection.java
@@ -0,0 +1,21 @@
+package com;
+import java.sql.DriverManager;
+import com.mysql.jdbc.Connection;
+
+class MysqlConnection{  
+public static void main(String args[]){  
+	Connection conn= null;
+	
+try{  
+	conn=(Connection)DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/aryan","root","root");  
+	  
+	if(conn!=null) {
+		  System.out.println("Connection Successfully connected to Database");
+	  }
+ 
+}
+catch(Exception e){ 
+	System.out.println("Not connected to Database");
+	}  
+}  
+}
